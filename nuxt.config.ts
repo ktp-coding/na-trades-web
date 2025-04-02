4b6eaabb1d91c5965723f7dc040da1a7521dd232
Na Krittaphat <na.k@finnomena.com>
2025-04-02 09:20:53 +0700

    add chart

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  rootDir: __dirname,
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
   css: ["~/assets/css/tailwind.css"],
-  typescript: {
-    strict: true,
-    typeCheck: true,
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
-  tailwindcss: {
-    exposeConfig: true,
-    viewer: true,
-  },
-  shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: "",
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: "./components/ui",
+  alias: {
+    "@": "~",
   },
 });
